3bb15af13aef146b84c135f9c347e5603d562f6b
yussif faisal <fekt2016@icloud.com>
2024-05-14 15:28:43 +0000

    update sponors

--- src/features/homepage/EventsTicket.jsx
+++ src/features/homepage/EventsTicket.jsx
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 import Section from '../../ui/Section'
 // import { devicesMax } from '../../styles/BreakPoint'
 
-const StyledTicket = styled.div`
-  padding: 6rem;
+const StyledEvent = styled.div`
+  /* padding: 6rem; */
   width: 100%;
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
   gap: 20px;
   justify-content: space-around;
   align-items: center;
@@ -14,34 +14,92 @@ const StyledTicket = styled.div`
 
   border-radius: 10px;
 `
+const EventCard = styled.div``
+const Header = styled.header`
+  width: 100%;
+  background-color: red;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+`
+const StyledSpan = styled.span`
+  background-color: green;
+  padding: 1rem;
+  text-align: center;
+  color: white;
+  font-family: 'Metal Mania', system-ui;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 2rem;
+`
+const StyledDate = styled.span`
+  position: absolute;
+  right: 10px;
+  bottom: 30px;
+  font-weight: bold;
+  font-size: 2rem;
+  background-color: white;
+  border-radius: 10px;
+  padding: 0.2rem 0.5rem;
+  transform: rotate(25deg);
+`
+
+const H1 = styled.h1`
+  font-family: 'Oleo Script Swash Caps', system-ui;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 5rem;
+  text-align: center;
+  -webkit-text-stroke: 5px transparent;
+  background: -webkit-linear-gradient(top, red, green);
+  background-clip: text;
+  color: gold;
+  text-shadow: 4px 4px 6px rgba(66, 68, 90, 1);
+`
+const Center = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  background-color: antiquewhite;
+`
 
 function EventsTicket() {
   return (
     <Section type="event">
-      <StyledTicket>
-        <header>
-          <h2>Ghana-Canada International </h2>
-          <span> Music and cultural festival </span>
-        </header>
+      <StyledEvent>
+        <div>1</div>
+        <EventCard>
+          <Center>
+            <Header>
+              <H1> Music and cultural festival</H1>
+              <StyledSpan> Ghana-bahamas International</StyledSpan>
+              <StyledDate>2024</StyledDate>
+            </Header>
 
-        <span>2024</span>
-
-        <div>
-          Fostering creativity through music and culture to promote Natinal
-          development and peace.
-        </div>
-        <div>29th - 31st march 2024</div>
-        <div>
-          10 belfield road, etoicoke,(kipling/belfield), toronto, Ontario,
-          M9WIGI, Canada
-        </div>
-        <div>register now </div>
-        <div>+16099137252||+233546009191|| +233244741674|| +41763663079 ||</div>
-        <div>
-          paa solo, kaakyere K.Appiah, Bessa Simons, Nana Gyinadu kuffour I
-          musical chief
-        </div>
-      </StyledTicket>
+            <div>
+              Fostering creativity through music and culture to promote Natinal
+              development and peace.
+            </div>
+            <div>29th - 31st march 2024</div>
+            <div>
+              10 belfield road, etoicoke,(kipling/belfield), toronto, Ontario,
+              M9WIGI, Canada
+            </div>
+            <div>register now </div>
+            <div>
+              +16099137252||+233546009191|| +233244741674|| +41763663079 ||
+            </div>
+            <div>
+              paa solo, kaakyere K.Appiah, Bessa Simons, Nana Gyinadu kuffour I
+              musical chief
+            </div>
+          </Center>
+        </EventCard>
+        <div>2</div>
+      </StyledEvent>
     </Section>
   )
 }
--- src/features/homepage/Sponsor.jsx
+++ src/features/homepage/Sponsor.jsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components'
+import Section from '../../ui/Section'
+
+const Container = styled.div`
+  padding: 6rem;
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const Img = styled.img`
+  height: 10rem;
+`
+
 function Sponsor() {
-  return <div></div>
+  return (
+    <Section>
+      <Container>
+        <Img src="../../../Geniuspring.png" alt="sponsors logo" />
+        <Img src="../../../Shoe.png" alt="sponsors logo" />
+        <Img src="../../../Toloba.png" alt="sponsors logo" />
+        <Img src="../../../V-Bells.png" alt="sponsors logo" />
+        <Img src="../../../Karim & Brother's.png" alt="sponsors logo" />
+      </Container>
+    </Section>
+  )
 }
 
 export default Sponsor
